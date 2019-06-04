afce55fa83c16d3a7a8df49a3ce5ebc6093fbeef
Jake Miller <jake0miller@gmail.com>
2019-06-04 09:23:24 -0600

    Finish iteration 2

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,4 +13,12 @@ class Curator
   def add_artist(artist)
     @artists << artist
   end
+
+  def find_photograph_by_id(id)
+    @photographs.find {|photo| photo.id == id}
+  end
+
+  def find_artist_by_id(id)
+    @artists.find {|artist| artist.id == id}
+  end
 end
